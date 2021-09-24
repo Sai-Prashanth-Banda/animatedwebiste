bf8474f7a469970b7d50761ad45a5b362dc01a9f
Sai Prashanth <65449720+Sai-Prashanth-Banda@users.noreply.github.com>
2021-09-24 09:50:04 +0530

    Add files via upload

---
+++ Hello.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './App.css'
+
+const Hello=()=>{
+    return(
+        <div>
+            <button type="button" class="third">Sai</button>
+</div>
+    )
+}
+export default Hello;
---
+++ index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+//import Routing from './Routing'
+import Hello from './Hello';
+
+
+ReactDOM.render(
+  <App/>,
+  document.getElementById('root')
+);
